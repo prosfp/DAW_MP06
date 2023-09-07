f8506d77afedf3fd0e226600351d4e79236dd920
prosfp <110301233+prosfp@users.noreply.github.com>
2023-09-07 11:55:21 +0200

    Exemples de codi teoria afegits

---
+++ assets/UF1_Codi_Teoria/PHP_Ex/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemple PHP</title>
+</head>
+<body>
+    <h1>Benvingut a l'exemple PHP</h1>
+
+    <?php
+    // Exemple de codi PHP que genera contingut dinàmic
+    $nom = "John";
+    $edat = 30;
+    
+    echo "<p>El meu nom és $nom i tinc $edat anys.</p>";
+    ?>
+
+    <p>Aquest és un paràgraf HTML estàtic.</p>
+</body>
+</html>
