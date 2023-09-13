91aef6c3444fd94ea557efbc3ac86310f8a92bbc
«Pablo <pros7@xtec.cat>
2023-09-13 10:03:29 +0200

    Exemples_teoria_inici_modificat

--- assets/UF1_Codi_Teoria/PHP_Ex/index.php
+++ assets/UF1_Codi_Teoria/PHP_Ex/index.php
@@ -1,19 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exemple PHP</title>
+    <title>Calculadora de Suma en PHP</title>
 </head>
 <body>
-    <h1>Benvingut a l'exemple PHP</h1>
-
-    <?php
-    // Exemple de codi PHP que genera contingut dinàmic
-    $nom = "John";
-    $edat = 30;
+    <h1>Calculadora de Suma en PHP</h1>
+    <form method="post">
+        <label for="num1">Número 1:</label>
+        <input type="number" id="num1" name="num1">
+        <br>
+        <label for="num2">Número 2:</label>
+        <input type="number" id="num2" name="num2">
+        <br>
+        <input type="submit" name="sumar" value="Sumar">
+    </form>
     
-    echo "<p>El meu nom és $nom i tinc $edat anys.</p>";
+    <?php
+    if (isset($_POST['sumar'])) {
+        // Obteniu els valors dels inputs
+        $num1 = floatval($_POST['num1']);
+        $num2 = floatval($_POST['num2']);
+        
+        // Realitza la suma
+        $resultat = $num1 + $num2;
+        
+        // Mostra el resultat
+        echo "<p>Resultat: $resultat</p>";
+    }
     ?>
-
-    <p>Aquest és un paràgraf HTML estàtic.</p>
 </body>
 </html>
