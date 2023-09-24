2fc8359324a1e7a5bc12b4cdfff3679d4b7fd54e
prosfp <110301233+prosfp@users.noreply.github.com>
2023-09-24 19:07:05 +0200

    proves tailwind

--- assets/UF1_Codi_Teoria/JS_Ex/sumar.js
+++ assets/UF1_Codi_Teoria/JS_Ex/sumar.js
@@ -1,11 +1,21 @@
 function sumar() {
   // Obtén els valors dels inputs
-  var num1 = parseFloat(document.getElementById('num1').value);
-  var num2 = parseFloat(document.getElementById('num2').value);
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
 
   // Realitza la suma
   var resultat = num1 + num2;
 
   // Mostra el resultat
-  document.getElementById('resultat').textContent = resultat;
+  document.getElementById("resultat").textContent = resultat;
 }
+
+// Create a function with regex that matches email address
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+console.log(validateEmail("pablo.ros@cirvianum.cat"));
+console.log(validateEmail("pablo"));
+console.log(validateEmail("pablo@cirvianum"));
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/Codis_Activitats/**/*.{html,js}"],
+  theme: {},
+  plugins: [],
+};
